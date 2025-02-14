50cfbac6aebfab31e85e78166a13cc3507bd33c6
Andy Stoffel <acstoffel@gmail.com>
2025-02-14 17:17:36 -0500

    task 6 - register and status codes for 2-5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,37 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(201).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -20,7 +47,11 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
     // get a book based on ISBN (we don't have real ISBNs - this is just the key)
     const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn],null,4));
+    
+    if(!books[isbn])
+        res.status(404).json({message: `No books found with ISBN ${isbn}`});
+    else
+        res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
@@ -29,23 +60,42 @@ public_users.get('/author/:author',function (req, res) {
   const author = req.params.author.toLowerCase();
   // Filter the books array to find books matching the extracted author parameter
   let books_array = Object.values(books).filter((book) => book.author.toLowerCase() === author);
-  res.send(JSON.stringify(books_array,null,4));
+  if(books_array.length == 0)
+        res.status(404).json({message: `No books found by author`});
+    else
+        res.status(200).json(books_array);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
    // Get the title from the request URL (space in title = %20)
-  const title = req.params.title.toLowerCase();
+  const title = req.params.title.toLowerCase().trim();
+
   // Filter the books array to find books matching the extracted author parameter
   // should really use a regex here
   let books_array = Object.values(books).filter((book) => book.title.toLowerCase() === title);
-  res.send(JSON.stringify(books_array,null,4));
+  
+  if(books_array.length == 0)
+        res.status(404).json({message: `No books found with title`});
+    else
+        res.status(200).json(books_array);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // get a book review based on ISBN  
+    const isbn = req.params.isbn;
+    let reviews =[];
+
+    // no reviews?
+    if(Object.keys(books[isbn]["reviews"]).length !== 0)
+        reviews.push(books[isbn]["reviews"]);
+    
+    if(reviews.length == 0)
+        res.status(404).json({message: `No reviews found for ISBN ${isbn}`});
+    else
+        res.status(200).json(reviews);
+
 });
 
 module.exports.general = public_users;
