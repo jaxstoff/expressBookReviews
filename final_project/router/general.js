95029d7c3351671de5500ad47023dcdcd5e94072
Andy Stoffel <acstoffel@gmail.com>
2025-02-14 15:30:36 -0500

    tasks 1,2,3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,24 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // get list of books in json
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // get a book based on ISBN (we don't have real ISBNs - this is just the key)
+    const isbn = req.params.isbn;
+    res.send(JSON.stringify(books[isbn],null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Get the author from the request URL (space in author name = %20)
+  const author = req.params.author.toLowerCase();
+  // Filter the books array to find books matching the extracted author parameter
+  let books_array = Object.values(books).filter((book) => book.author.toLowerCase() === author);
+  res.send(JSON.stringify(books_array,null,4));
 });
 
 // Get all books based on title
